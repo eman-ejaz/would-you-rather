de925952acb06179ab7aa890d515e5998f772323
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2022-04-06 02:43:15 +0500

    feat: Add LeadBar component

---
+++ src/components/LeaderBoard.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {useSelector} from "react-redux";
+import {createLeaderBoardData} from "../utils/utils";
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import Paper from '@mui/material/Paper';
+import {Redirect} from "react-router-dom";
+import {ROUTE_URLS} from "../constants/routes";
+import Navbar from "./Navbar";
+import {Typography} from "@mui/material";
+
+const LeaderBoard = () => {
+    const users = useSelector((state) => state.usersReducer.users);
+    const {authedUser} = useSelector((state) => state.authedReducer);
+    const loggedIn = authedUser !== null;
+
+    if (!loggedIn) {
+        return <Redirect to={ROUTE_URLS.LOGIN}/>;
+    }
+    return (
+        <>
+            <Navbar/>
+            <Typography variant={'h4'} component={'div'} gutterBottom>
+                Leaders of Would you Rather.
+            </Typography>
+            <TableContainer component={Paper}>
+                <Table sx={{minWidth: 650}} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>User</TableCell>
+                            <TableCell align="right">Questions</TableCell>
+                            <TableCell align="right">Answers</TableCell>
+                            <TableCell align="right">Score</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {createLeaderBoardData(users).map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">{row.questionsScore}</TableCell>
+                                <TableCell align="right">{row.answersScore}</TableCell>
+                                <TableCell align="right">{row.leaderBoardScore}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
+}
+
+export default LeaderBoard
