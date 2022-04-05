9ec7f9db102a281d3e2e15270f5bc5715f6abbff
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2022-04-06 02:40:09 +0500

    feat: Add redux functionality to answer a question and create a new one

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,11 +1,29 @@
-import { getInitialData } from '../utils/apiCalls';
-import { receiveQuestions } from './questions';
-import { receiveUsers } from './users';
+import {getInitialData, saveQuestion, saveQuestionAnswer} from '../utils/apiCalls';
+import {createNewQuestion, receiveQuestions, saveAnswerQuestion} from './questions';
+import {receiveUsers, saveUserAnswer, saveUserQuestion} from './users';
 
 export const handleInitialData = () => {
-  return async (dispatch) => {
-    const { users, questions } = await getInitialData();
-    dispatch(receiveUsers(users));
-    dispatch(receiveQuestions(questions));
-  };
+    return async (dispatch) => {
+        const {users, questions} = await getInitialData();
+        dispatch(receiveUsers(users));
+        dispatch(receiveQuestions(questions));
+    };
 };
+
+export const handleSaveAnswer = (authedUser, questionId, userAnswer) => {
+    return async (dispatch) => {
+        await saveQuestionAnswer(authedUser, questionId, userAnswer)
+
+        dispatch(saveAnswerQuestion(questionId, userAnswer, authedUser))
+        dispatch(saveUserAnswer(questionId, userAnswer, authedUser))
+
+    }
+}
+
+export const handleSaveQuestion = (optionOne, optionTwo, authedUser) => {
+    return async (dispatch) => {
+       const savedQuestion = await saveQuestion({author: authedUser, optionOneText: optionOne, optionTwoText: optionTwo})
+        dispatch(createNewQuestion(savedQuestion))
+        dispatch(saveUserQuestion(savedQuestion.id, authedUser))
+    }
+}
