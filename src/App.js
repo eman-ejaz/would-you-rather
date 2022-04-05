676e9346af769e649dfcaff27dc9d5f04599012a
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2022-04-06 02:42:38 +0500

    feat: Add functionality to app root components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useDispatch } from 'react-redux';
+import {useEffect} from 'react';
+
+import { bindActionCreators } from 'redux';
+
+import { sharedActionCreators } from './actions/index';
+
+import routes from './routes'
+
 function App() {
+  const dispatch = useDispatch();
+
+  const { handleInitialData } = bindActionCreators(
+      sharedActionCreators,
+      dispatch
+  );
+
+  useEffect(() => {
+    handleInitialData();
+  }, [handleInitialData]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {routes}
     </div>
   );
 }
