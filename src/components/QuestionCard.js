d3b3dd898f4f6604303581eff196ed1be20d8a33
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2022-04-06 02:38:58 +0500

    feat: Add Question Card component

---
+++ src/components/QuestionCard.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import { Card, Grid, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
+
+
+const QuestionCard = ({userAvatar, question}) => {
+    return (
+        <Card variant="outlined">
+            <Grid container>
+                <Grid item xs={12}>
+                    <Typography variant='overline' gutterBottom>
+                        Eman asks:
+                    </Typography>
+                </Grid>
+                <Grid item xs={5}>
+                    <img src={userAvatar} alt={'Avatar'}/>
+                </Grid>
+                <Grid container item xs={7}>
+                    <Grid item xs={12}>
+                        <Typography variant='subtitle1' gutterBottom>
+                            Would you rather
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant='subtitle1' component='div' gutterBottom>
+                            {question.optionOne.text}
+                            {' '} or ...
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                            <Button
+                                variant='contained'
+                                size='small'
+                                color='primary'
+                                component={Link}
+                                to={`/questions/${question.id}`}
+                            >
+                                Open Question
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Card>
+
+
+    )
+}
+
+export default QuestionCard
